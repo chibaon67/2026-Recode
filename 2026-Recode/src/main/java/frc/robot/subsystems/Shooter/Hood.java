a8c176d47f0e6ec37f273c7f4c43a75b40d8f883
Chibao Nguyen <chibaon678@gmail.com>
2026-07-09 21:12:15 -0500

    shooter commands

--- 2026-Recode/src/main/java/frc/robot/subsystems/Shooter/Hood.java
+++ 2026-Recode/src/main/java/frc/robot/subsystems/Shooter/Hood.java
@@ -1,5 +1,33 @@
 package frc.robot.subsystems.Shooter;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.Command;
 
-public class Hood {
+public class Hood extends SubsystemBase{
+    private final TalonFX hoodMotor;
+    public Hood(int motorID){
+        hoodMotor = new TalonFX(motorID);
+    }
+
+    public void adjustHood(boolean down){
+        if(down){
+            hoodMotor.set(0.3);
+        }
+        else{
+            hoodMotor.set(-0.3);
+        }
+    }
+
+    public Command upCommand(){
+        return new RunCommand(()->adjustHood(false),this);
+    }
     
+    public Command downCommand(){
+        return new RunCommand(()->adjustHood(true),this);
+    }
+
+
 }
--- 2026-Recode/src/main/java/frc/robot/subsystems/Shooter/Turret.java
+++ 2026-Recode/src/main/java/frc/robot/subsystems/Shooter/Turret.java
@@ -1,5 +1,30 @@
 package frc.robot.subsystems.Shooter;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.Command;
 
-public class Turret {
-    
+public class Turret extends SubsystemBase{
+    private final TalonFX turretMotor;
+    public Turret(int motorID){
+        turretMotor = new TalonFX(motorID);
+    }
+    public void adjustTurret(boolean left){
+        if(left){
+            turretMotor.set(0.3);
+        }
+        else{
+            turretMotor.set(-0.3);
+        }
+    }
+
+    public Command rightCommand(){
+        return new RunCommand(()->adjustTurret(false), this);
+    }
+
+    public Command leftCommand(){
+        return new RunCommand(()->adjustTurret(true),this);
+    }
 }
