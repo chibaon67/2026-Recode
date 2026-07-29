d80406010a37fd6b8001b914e2fc2e767cd3574a
Chibao Nguyen <chibaon678@gmail.com>
2026-07-29 11:20:21 -0500

    starting pid

--- src/main/java/frc/robot/subsystems/Shooter/Turret.java
+++ src/main/java/frc/robot/subsystems/Shooter/Turret.java
@@ -5,11 +5,16 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.math.controller.PIDController;
 
 public class Turret extends SubsystemBase{
-    private final TalonFX turretMotor;
+    private final TalonFX turretMotor; 
+    private static final double GEAR_RATIO = (11.0*10.0) / (50.0 * 83.0);
+    private final PIDController turretPID;
+    
     public Turret(int motorID){
         turretMotor = new TalonFX(motorID);
+        PIDController pid = new PIDController
     }
     public void adjustTurret(boolean left){
         if(left){
